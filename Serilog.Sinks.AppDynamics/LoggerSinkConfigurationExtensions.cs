3e2a7a01d503784a7b451144f33ed708b27cdbf9
Seány <seanmacdomhnaill@gmail.com>
2017-05-18 01:23:47 +0100

    Comments for SinkConfiguration

--- Serilog.Sinks.AppDynamics/LoggerSinkConfigurationExtensions.cs
+++ Serilog.Sinks.AppDynamics/LoggerSinkConfigurationExtensions.cs
@@ -7,12 +7,22 @@ namespace Serilog.Sinks.AppDynamics
 {
     public static class LoggerSinkConfigurationExtensions
     {
+        /// <summary>
+        /// Sink configuration for AppDynamics integration.
+        /// </summary>
+        /// <param name="sinkConfiguration"></param>
+        /// <param name="requestUri">AppDynamics API endpoint.</param>
+        /// <param name="batchPostingLimit">The maximum number of events to be batched before sync.</param>
+        /// <param name="periodBatchSyncs">Time to wait before batch occurs.</param>
+        /// <param name="eventBodyLimitBytes">Max byte size before message is discarded. Default is 256KB</param>
+        /// <param name="httpClient">HttpClient for issuing HTTP POST requests.</param>
+        /// <returns>A SeriLog LoggerConfiguration extension hook.</returns>
         public static LoggerConfiguration AppDynamics(
             this LoggerSinkConfiguration sinkConfiguration,
             string requestUri,
             int batchPostingLimit = 1000,
             TimeSpan? periodBatchSyncs = null,
-            long? eventBodyLimitBytes = 256 * 1024,
+            long? eventBodyLimitBytes = 0x40000,
             IHttpClient httpClient = null)
         {
             if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
